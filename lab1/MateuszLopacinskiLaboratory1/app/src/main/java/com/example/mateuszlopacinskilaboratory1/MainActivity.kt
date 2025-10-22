22089999d09fc56bdade5f453ba0cc6aa519c6c4
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-10-22 12:12:00 +0200

    Finish introduction task

--- lab1/MateuszLopacinskiLaboratory1/app/src/main/java/com/example/mateuszlopacinskilaboratory1/MainActivity.kt
+++ lab1/MateuszLopacinskiLaboratory1/app/src/main/java/com/example/mateuszlopacinskilaboratory1/MainActivity.kt
@@ -10,15 +10,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -27,11 +31,51 @@ class MainActivity : ComponentActivity() {
     enableEdgeToEdge()
     setContent {
       val ctx = LocalContext.current
-      Column {
+      val txt: MutableState<String> = remember {
+        mutableStateOf("This text will be changed")
+      }
+      val shouldTextsBePresented: MutableState<Boolean> = remember {
+        mutableStateOf(false)
+      }
+      val tfValue: MutableState<String> = remember {
+        mutableStateOf("My value")
+      }
+
+      Column(modifier = Modifier
+        .statusBarsPadding()
+        .navigationBarsPadding()) {
+        Button(onClick = {
+          Log.i("myapp", "Entry from my application")
+        }) {
+          Text("Log to LogCat")
+        }
+
         Button(onClick = {
           Toast.makeText(ctx, "Hello there!", LENGTH_LONG).show()
-        }, modifier = Modifier.height(100.dp)) {
+        }) {
           Text("My first button")
+        }
+
+        Button(onClick = {
+          txt.value = "My new text"
+        }) {
+          Text("Change the text")
+        }
+
+        Text(text = txt.value, fontSize = 20.sp)
+
+        Button(onClick = {
+          shouldTextsBePresented.value = !shouldTextsBePresented.value
+        }) {
+          Text("${if (shouldTextsBePresented.value) "Hide" else "Display"} colored text")
+        }
+
+        TextField(value = tfValue.value, onValueChange = {
+          tfValue.value = it
+        })
+
+        if (shouldTextsBePresented.value) {
+          MyTexts()
         }
       }
     }
@@ -41,8 +85,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyTexts() {
   Column(
-    verticalArrangement = Arrangement.SpaceEvenly,
     horizontalAlignment = Alignment.End,
+    verticalArrangement = Arrangement.Bottom,
     modifier = Modifier.fillMaxSize()
   ) {
     Text(text = "Hello World!", color = Color.Red, fontSize = 20.sp, letterSpacing = 3.sp)
