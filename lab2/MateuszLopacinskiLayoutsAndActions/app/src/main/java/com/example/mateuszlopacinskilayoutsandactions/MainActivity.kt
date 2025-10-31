a332122002a6cb2890d579aa86eb06f565c33637
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-10-31 22:25:06 +0100

    Finish part A, start part B

--- lab2/MateuszLopacinskiLayoutsAndActions/app/src/main/java/com/example/mateuszlopacinskilayoutsandactions/MainActivity.kt
+++ lab2/MateuszLopacinskiLayoutsAndActions/app/src/main/java/com/example/mateuszlopacinskilayoutsandactions/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.example.mateuszlopacinskilayoutsandactions
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mateuszlopacinskilayoutsandactions.databinding.ActivityActionsBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityActionsBinding
+  private lateinit var user: User
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    setContentView(R.layout.layout_weekend)
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.weekend)) { v, insets ->
+
+    binding = ActivityActionsBinding.inflate(layoutInflater)
+//    user = User("Mateusz", "Łopaciński")
+//    binding.user = user
+    setContentView(binding.root)
+
+    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_actions)) { v, insets ->
       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
       insets
     }
+    setWriteToLogCatButtonListener()
+    setDisplayToastButtonListener()
+    setWriteToTextViewButtonListener()
+  }
+
+  fun writeToLogCat(view: View) {
+    Log.i("MyApp", "Message from my App")
+  }
+
+  private fun setWriteToLogCatButtonListener() {
+    binding.btnWriteToLogcat.setOnClickListener {
+      Log.i("MyApp", "Message from the BindListener")
+    }
+  }
+
+  private fun setDisplayToastButtonListener() {
+    binding.btnDisplayToast.setOnClickListener {
+      Toast.makeText(this, "This is a Toast message", Toast.LENGTH_SHORT).show()
+    }
+  }
+
+  private fun setWriteToTextViewButtonListener() {
+//    binding.btnWriteToTextview.setOnClickListener {
+//      binding.tvWriteSomething.text = getString(R.string.i_love_this_game)
+//    }
   }
 }
---
+++ lab2/MateuszLopacinskiLayoutsAndActions/app/src/main/java/com/example/mateuszlopacinskilayoutsandactions/User.kt
@@ -0,0 +1,6 @@
+package com.example.mateuszlopacinskilayoutsandactions
+
+data class User(
+  var firstName: String,
+  var lastName: String,
+)
